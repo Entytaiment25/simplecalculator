67658a81138053538d16454cb8867ab6af03c700
Entytaiment25 <entytaiment25@outlook.com>
2025-09-29 17:34:29 +0200

    cleanup and added build directory

--- build.zig
+++ build.zig
@@ -3,7 +3,6 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-
     const exe = b.addExecutable(.{
         .name = "simplecalculator",
         .root_module = b.createModule(.{
@@ -18,18 +17,6 @@ pub fn build(b: *std.Build) void {
     const run_artifact = b.addRunArtifact(exe);
     run_artifact.step.dependOn(b.getInstallStep());
 
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_artifact.step);
-
-    if (b.args) |args| {
-        run_artifact.addArgs(args);
-    }
-
-    const test_exe = b.addTest(.{
-        .root_module = exe.root_module,
-    });
-    const run_test = b.addRunArtifact(test_exe);
-
-    const test_step = b.step("test", "Run tests");
-    test_step.dependOn(&run_test.step);
+    b.step("run", "Run the app").dependOn(&run_artifact.step);
+    if (b.args) |args| run_artifact.addArgs(args);
 }
